be118c68c33f105fb9560d94fb5e2f83aa2d080e
Dulatea <dulatea@MacBook-Air-Dulatea.local>
2026-02-02 22:45:50 +0500

    Update feature name to 'Состояние души' (Soul State)

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -364,7 +364,7 @@ class AppLocalizations {
       'brainDevelopmentFeature': 'Ми дамуы',
       'gamesAndActivitiesFeature': 'Ойындар және белсенділік',
       'trafficLightFeature': 'Бағдаршам',
-      'stateAnalyticsFeature': 'Жан талдауы',
+      'stateAnalyticsFeature': 'Жан күйі',
       'psychologistFeature': 'Психолог',
       'canChangeLayerInSettings': 'Мұны кейін параметрлерде өзгертуге болады',
       
@@ -709,7 +709,7 @@ class AppLocalizations {
       'brainDevelopmentFeature': 'Развитие мозга',
       'gamesAndActivitiesFeature': 'Игры и активности',
       'trafficLightFeature': 'Светофор',
-      'stateAnalyticsFeature': 'Аналитика души',
+      'stateAnalyticsFeature': 'Состояние души',
       'psychologistFeature': 'Психолог',
       'canChangeLayerInSettings': 'Вы можете изменить это позже в настройках',
       
@@ -1054,7 +1054,7 @@ class AppLocalizations {
       'brainDevelopmentFeature': 'Brain Development',
       'gamesAndActivitiesFeature': 'Games & Activities',
       'trafficLightFeature': 'Traffic Light',
-      'stateAnalyticsFeature': 'Soul Analytics',
+      'stateAnalyticsFeature': 'Soul State',
       'psychologistFeature': 'Psychologist',
       'canChangeLayerInSettings': 'You can change this later in settings',
       
